17de9cfbea92802ff005d0b2117bcaff547a519e
Ivan Demchenko <vanuademchenko@gmail.com>
2021-03-26 17:11:01 +0300

    Setup labels with information of colors, set sliders functions and main logic of app

--- AppRGB/ViewController.swift
+++ AppRGB/ViewController.swift
@@ -15,19 +15,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var colorView: UIView!
     
+    @IBOutlet weak var valueOfRedSlider: UILabel!
+    @IBOutlet weak var valueOfGreenSlider: UILabel!
+    @IBOutlet weak var valueOfBlueSlider: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        colorView.layer.cornerRadius = 20
+        setColorsOfSliders()
+        showValueOfSliders()
     }
     
     @IBAction func redSliderSlide() {
         changeColorOfView()
+        showValueOfSliders()
     }
     @IBAction func greenSliderSlide() {
         changeColorOfView()
+        showValueOfSliders()
     }
     @IBAction func blueSliderSlide() {
         changeColorOfView()
+        showValueOfSliders()
     }
     
     
@@ -35,6 +45,22 @@ class ViewController: UIViewController {
     private func changeColorOfView() {
         colorView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
+    private func setColorsOfSliders() {
+        redSlider.minimumTrackTintColor = .red
+        redSlider.thumbTintColor = .red
+        
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.thumbTintColor = .green
+        
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.thumbTintColor = .blue
+    }
+    private func showValueOfSliders() {
+        valueOfRedSlider.text = String(round(100 * redSlider.value) / 100)
+        valueOfGreenSlider.text = String(round(100 * greenSlider.value) / 100)
+        valueOfBlueSlider.text = String(round(100 * blueSlider.value) / 100)
+    }
 
+    
 }
 
